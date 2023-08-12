5a4284ff4295b5aa92d4e187eea68d0d38c7f0ea
kkumato <yvainetomato@gmail.com>
2023-08-12 22:02:37 +0800

    icons page, signin page styling

---
+++ assets/icons.js
@@ -0,0 +1,31 @@
+import userIcon from "./user.svg";
+import emailIcon from "./email.svg";
+import passwordIcon from "./password.svg";
+import googleIcon from "./googleIcon.svg";
+import facebookIcon from "./facebookIcon.svg";
+import appleIcon from "./appleIcon.svg";
+import logoIcon from "./logo.svg";
+import languageIcon from "./languageIcon.svg";
+import settingsIcon from "./settingsIcon.svg";
+import contactIcon from "./contactIcon.svg";
+import feedbackIcon from "./feedbacklcon.svg";
+import privacyIcon from "./privacyIcon.svg";
+import aboutIcon from "./aboutIcon.svg";
+import logoBW from "./logoBW.svg";
+
+export {
+  userIcon,
+  emailIcon,
+  facebookIcon,
+  passwordIcon,
+  googleIcon,
+  appleIcon,
+  logoIcon,
+  languageIcon,
+  settingsIcon,
+  contactIcon,
+  feedbackIcon,
+  privacyIcon,
+  aboutIcon,
+  logoBW,
+};
